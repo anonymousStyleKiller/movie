ec2c94ab66b8b303c4f3b220b0eacc445250481f
Anton Kharchenko <gumb1t97@gmail.com>
2020-03-13 19:54:00 +0300

    data flow

--- src/App.js
+++ src/App.js
@@ -3,32 +3,58 @@ import './App.css';
 import moviesData from "./moviesData";
 import MovieItem from "./MovieItem";
 
-
+//UI = fn(state, props)
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: moviesData
-        }    }
+            movies: moviesData,
+            moviesWillWatch: []
+        }
+    }
 
     removeMovie = movie => {
         const updateMovie = this.state.movies.filter(function (item) {
             return item.id !== movie.id;
         });
         console.log(updateMovie);
-        this.setState({
-            movie: updateMovie
+        this.setState(
+            {
+            movies: updateMovie
         });
-    }
+    };
+
+    addMovieToWillWatch = movie => {
+        const updateWillWatch = [...this.state.moviesWillWatch, movie];
+        this.setState(
+            {
+                moviesWillWatch: updateWillWatch
+            });
+    };
 
     render() {
         return (
-            <div>
-                {this.state.movies.map((movie) => {
-                    return (<MovieItem key={movie.id}
-                                       movie={movie}
-                                       removeMovie={this.removeMovie}/>)
-                })}
+            <div className="container">
+                <div className="row">
+                    <div className="col-9">
+                        <div className="row">
+                            {this.state.movies.map((movie) => {
+                                return (
+                                    <div className="col-6 mb-4" key={movie.id}>
+                                        <MovieItem movie={movie}
+                                                   removeMovie={this.removeMovie}
+                                                   addMovieToWillWatch={this.addMovieToWillWatch}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <p>Will Watch: {this.state.moviesWillWatch.length}</p>
+                    </div>
+
+                </div>
             </div>
         );
     }
--- src/MovieItem.jsx
+++ src/MovieItem.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-let MovieItem = (props) => {
+
+
+const MovieItem = props => {
+    const {movie, removeMovie, addMovieToWillWatch} = props;
     return (
-        <div >
-            <p>{props.movie.title}</p>
-            <button onClick={props.removeMovie.bind(this, props.movie)}>
-                Delete movie
-            </button>
+        <div className="card">
+            <img className="card-img-top"
+                 src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
+                 alt=""/>
+            <div className="card-body">
+                <div className="card-title">{movie.title}</div>
+                <div className="d-flex justify-content-between align-items-center">
+                    <p className="mb-0">Rating: {movie.vote_average}</p>
+                    <button type="button" className="btn btn-secondary" onClick={addMovieToWillWatch.bind(null, movie)}>
+                        Will Watch
+                    </button>
+                </div>
+
+                <button onClick={removeMovie.bind(null, movie)}>
+                    Delete movie
+                </button>
+            </div>
         </div>
     );
 };
